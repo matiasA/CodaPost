e52710c7f9f08b5e1a7d20e0acbd2f2caca356ad
Matias A <matiasprogramador@gmail.com>
2024-08-15 12:58:54 -0300

    Corregir error de declaración múltiple de Coda_Logger- Añadir comprobación de existencia de clase en class-coda-logger.php- Asegurar que class-coda-logger.php se incluya solo una vez en class-coda-post.php- Eliminar cualquier declaración duplicada de Coda_Logger en otros archivos

--- includes/class-post-publisher.php
+++ includes/class-post-publisher.php
@@ -1,28 +1,30 @@
 <?php
 
-class Coda_Logger {
-    private $log_file;
+if (!class_exists('Coda_Logger')) {
+    class Coda_Logger {
+        private $log_file;
 
-    public function __construct() {
-        $this->log_file = plugin_dir_path(dirname(__FILE__)) . 'coda-post.log';
-    }
+        public function __construct() {
+            $this->log_file = plugin_dir_path(dirname(__FILE__)) . 'coda-post.log';
+        }
 
-    public function log($message, $level = 'INFO') {
-        $timestamp = date('Y-m-d H:i:s');
-        $log_entry = "[$timestamp] [$level] $message" . PHP_EOL;
-        
-        file_put_contents($this->log_file, $log_entry, FILE_APPEND);
-    }
+        public function log($message, $level = 'INFO') {
+            $timestamp = date('Y-m-d H:i:s');
+            $log_entry = "[$timestamp] [$level] $message" . PHP_EOL;
+            
+            file_put_contents($this->log_file, $log_entry, FILE_APPEND);
+        }
 
-    public function info($message) {
-        $this->log($message, 'INFO');
-    }
+        public function info($message) {
+            $this->log($message, 'INFO');
+        }
 
-    public function error($message) {
-        $this->log($message, 'ERROR');
-    }
+        public function error($message) {
+            $this->log($message, 'ERROR');
+        }
 
-    public function warning($message) {
-        $this->log($message, 'WARNING');
+        public function warning($message) {
+            $this->log($message, 'WARNING');
+        }
     }
 }
